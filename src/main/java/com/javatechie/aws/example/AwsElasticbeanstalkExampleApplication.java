30edd8ea4ec49540d205606b7183ef78d87769c2
Basant kumar Hota <basahota@users.noreply.github.com>
2020-06-30 23:18:50 +0530

    #code

---
+++ src/main/java/com/javatechie/aws/example/AwsElasticbeanstalkExampleApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.aws.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AwsElasticbeanstalkExampleApplication {
+
+    @GetMapping("/status")
+    public String deploy() {
+        return "Application deployed to AWS beanstalk....";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsElasticbeanstalkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatechie/aws/example/AwsElasticbeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.aws.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticbeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
